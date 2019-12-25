e252964dc418454c80fef80356d957606174322a
geethasohita <geethasohita@gmail.com>
2019-12-24 21:15:33 -0600

    Modified greet to post

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-app=Flask(__name__)
+
+app = Flask(__name__)
+
 
 @app.route('/hello')
 def hello():
     return "Hello World"
 
-@app.route('/greet')
-def greet():
-    return "how are you doing"
+
+@app.route('/greet/<name>',methods =['POST'])
+def greet(name):
+    return "how are you doing " + name
