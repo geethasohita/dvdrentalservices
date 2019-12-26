7de5b9219c2e3f33cc63871750adb6556ef050a7
geethasohita <geethasohita@gmail.com>
2019-12-25 19:16:32 -0600

    Added 2 endpoints and a customer instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+
+from app.Customer import Customer
 
 app = Flask(__name__)
 
@@ -11,3 +13,14 @@ def hello():
 @app.route('/greet/<name>',methods =['POST'])
 def greet(name):
     return "how are you doing " + name
+
+@app.route('/createcustomer',methods=['POST'])
+def createcustomer():
+    content=request.get_json()
+    print(content)
+    return content
+
+@app.route('/getcustomer/<firstname>',methods=['GET'])
+def getcustomer(firstname):
+    newcustomer=Customer(firstname,firstname,25)
+    return jsonify(newcustomer.__dict__)
